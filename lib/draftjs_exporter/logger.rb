1cb05cbe302e558aa7eb64cd11d112e6e71f08d6
Neck Varentsov <neck.varentsov@gmail.com>
2017-03-23 22:16:43 +0300

    use NullLogger inherited of std Logger

--- lib/draftjs_exporter/logger.rb
+++ lib/draftjs_exporter/logger.rb
@@ -1,5 +1,17 @@
 module DraftjsExporter
   class << self
     attr_accessor :logger
+
+    def logger
+      @logger ||= NullLogger.new
+    end
+  end
+
+  class NullLogger < Logger
+    def initialize(*_)
+    end
+
+    def add(*_, &__)
+    end
   end
 end
